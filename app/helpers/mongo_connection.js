aaf5139571aa9c18f9efd3636e99619c6166ee01
mby12 <47452597+mby12@users.noreply.github.com>
2023-11-23 16:34:42 +0700

    env

--- app/helpers/mongo_connection.js
+++ app/helpers/mongo_connection.js
@@ -1,4 +1,4 @@
 const { MongoClient } = require('mongodb');
 
-const globalMongoConnection = new MongoClient(`mongodb://root92441:19661fa1134f5ca8eef87ab8b4778850c68d3582b19973447b1b408031ba7686@mongo_server:27017`);
+const globalMongoConnection = new MongoClient(process.env.MONGO_DSN);
 module.exports = globalMongoConnection;
