a8b2a7844ed1bb648a3200e543e5236d4be0d43d
mby12 <47452597+mby12@users.noreply.github.com>
2023-12-01 14:41:31 +0700

    fix mongo

--- app/helpers/mongo_database.js
+++ app/helpers/mongo_database.js
@@ -1,8 +1,3 @@
-// // import { MongoClient } from 'mongodb';
-
 const globalMongoConnection = require("./mongo_connection");
 
-// import globalMongoClient from "./mongo_client.js";
-
-// export default globalMongoClient.db("bca_access_token");
-module.exports = globalMongoConnection.db("face_recognition");
+module.exports = globalMongoConnection.db(process.env.MONGO_DATABASE_NAME);
